4e8c17e5d6d45b75773e799ef9e18099196a8e55
Nessrine004 <nessrine.bekri@emsi-edu.ma>
2025-12-22 15:33:12 +0100

    TD01 : Ajouter un composant Header dans la page

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
+function Header() {
+  return (
+    <header style={{ textAlign: "center", padding: "20px" }}>
+      <img
+        src="/app_logo.webp"
+        alt="Logo de la formation"
+        style={{
+          width: "220px",   
+          marginBottom: "20px"
+        }}
+      />
+      <h1>Introduction à React</h1>
+      <h3>À la découverte des premières notions de React</h3>
+    </header>
+  );
+}
+
+
+function Content() {
   const [count, setCount] = useState(0)
 
   return (
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
+    </div>
+  );
+}
+
+function App() {
+
+  return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
+      <Header />
+
       <h1>MBDS</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
